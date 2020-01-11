10bd7f0a81b507be39a9013d5b84ebec0cdca0cc
Paulo Henrique <paulo.henrique06@hotmail.com>
2020-01-11 17:33:26 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('form')
+})
+
+app.listen(3000, err => {
+    if(err) console.log('Unable to start node server!')
+    else console.log('Server running...')
+})
+

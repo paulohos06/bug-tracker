88e5eec69cf64848503fba28c2654b5978ff1fd2
Paulo Henrique <paulo.henrique06@hotmail.com>
2020-01-13 23:44:40 -0200

    BugTracker v1.1.0

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
 const GoogleSpreadsheet = require('google-spreadsheet')
 const credentials = require('./bugtracker.json')
-
-// spreadsheet config
-const docId = '1Lv8lsJPv5w1b_HXpwY6ERH47HLfO1NMLXCPY6gzeMjU'
+const { promisify } = require('util')
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -14,36 +13,35 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('form')
+    res.render('form', { successMsg: false })
 })
 
-app.post('/', (req, res) => {
-    const { name, email, howToReproduce, expectedOutput, receivedOutput } = req.body
-    const googleDoc = new GoogleSpreadsheet(docId)
-
-    googleDoc.useServiceAccountAuth(credentials, err => {
-        if(err) {
-            console.log('Unable to open spreadsheet')
-        } else {
-            googleDoc.getInfo((err, info) => {
-                let worksheet = info.worksheets[0]
-                worksheet.addRow({
-                    name: name,
-                    email: email,
-                    howToReproduce: howToReproduce,
-                    expectedOutput: expectedOutput,
-                    receivedOutput: receivedOutput
-                }, err => {
-                    if(err) res.send('Unable to report this bug!')
-                    else res.send('Bug reported!!!')
-                })
-            })
-        }
-    })
-
+app.post('/', async (req, res) => {
+    try {
+        const { name, email, howToReproduce, expectedOutput, receivedOutput, userAgent, userDate, issueType } = req.body
+        const googleDoc = new GoogleSpreadsheet('1Lv8lsJPv5w1b_HXpwY6ERH47HLfO1NMLXCPY6gzeMjU')
+        await promisify(googleDoc.useServiceAccountAuth)(credentials)
+        const info = await promisify(googleDoc.getInfo)()
+        const worksheet = info.worksheets[0]
+        await promisify(worksheet.addRow)({
+            name,
+            email,
+            howToReproduce,
+            expectedOutput,
+            receivedOutput,
+            userAgent,
+            userDate,
+            issueType,
+            source: req.query.source || 'direct'
+        })
+        res.render('form', { successMsg: 'Solicitação feita com sucesso!' })
+    } catch(err) {
+        res.send('Erro ao enviar o formulário.')
+        console.log(err)
+    }
 })
 
-app.listen(3000, err => {
+app.listen(port, err => {
     if(err) console.log('Unable to start node server!')
     else console.log('Server running...')
 })
